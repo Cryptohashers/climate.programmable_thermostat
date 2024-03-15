2b470e05ec92473e1b45c5c20febd77f3f6e3761
Cryptohashers <137129539+Cryptohashers@users.noreply.github.com>
2024-03-15 14:25:19 -0400

    Update const.py

--- custom_components/programmable_thermostat/const.py
+++ custom_components/programmable_thermostat/const.py
@@ -15,17 +15,24 @@ DEFAULT_NAME = 'Programmable Thermostat'
 DEFAULT_MAX_TEMP = 40
 DEFAULT_MIN_TEMP = 5
 DEFAULT_HVAC_OPTIONS = 7
+DEFAULT_FAN_OPTIONS = 3
 DEFAULT_AUTO_MODE = 'all'
 DEFAULT_MIN_CYCLE_DURATION = ''
 
+
 #Others
 MAX_HVAC_OPTIONS = 8
+MAX_FAN_OPTIONS = 4
 AUTO_MODE_OPTIONS = ['all', 'heating', 'cooling']
 INITIAL_HVAC_MODE_OPTIONS = ['', HVACMode.COOL, HVACMode.HEAT, HVACMode.OFF, HVACMode.HEAT_COOL]
 INITIAL_HVAC_MODE_OPTIONS_OPTFLOW = ['null', HVACMode.COOL, HVACMode.HEAT, HVACMode.OFF, HVACMode.HEAT_COOL]
 REGEX_STRING = r'((?P<hours>\d+?):(?=(\d+?:\d+?)))?((?P<minutes>\d+?):)?((?P<seconds>\d+?))?$'
 
 #Attributes
-ATTR_HEATER_IDS = "heater_ids"
-ATTR_COOLER_IDS = "cooler_ids"
+ATTR_HEATER_HIGH_IDS = "heater_ids"
+ATTR_HEATER_MED_IDS = "heater_ids"
+ATTR_HEATER_LOW_IDS = "heater_ids"
+ATTR_COOLER_HIGH_IDS = "cooler_ids"
+ATTR_COOLER_MED_IDS = "cooler_ids"
+ATTR_COOLER_LOW_IDS = "cooler_ids"
 ATTR_SENSOR_ID = "sensor_id"
